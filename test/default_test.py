46b0ce956e77c23d587d452bf162160561dbc50b
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2023-10-07 18:51:26 +0300

    1. Дописан метод solve в соответствии с требованием к ДЗ 2. Добавлены тесты в соответствии с ДЗ

--- test/default_test.py
+++ test/default_test.py
@@ -1,12 +1,39 @@
-# import pytest
+import pytest
 
 from solver import Solver
 
+
 def test_no_root():
-      assert Solver.solve(a=1, b=0, c=1) == None 
+    assert Solver.solve(a=1, b=0, c=1) == []
 
-def test_two_equal_root():
-      roots = Solver.solve(a=1, b=0, c=-1)
-      assert len(roots) == 2
-      assert roots[0] != roots[1]
 
+def test_two_non_equal_roots():
+    roots = Solver.solve(a=1, b=0, c=-1)
+    assert len(roots) == 2
+    assert roots[0] == -1 and roots[1] == 1
+
+
+def test_two_equal_roots():
+    roots = Solver.solve(a=1, b=2, c=1)
+    assert len(roots) == 2
+    assert roots[0] == roots[1] == -1
+
+
+def test_too_small_a():
+    with pytest.raises(ZeroDivisionError):
+        Solver.solve(0, 2, 1)
+        Solver.solve(-10e-7, 2, 1)
+
+
+def test_too_small_D():
+    assert Solver.solve(a=1, b=-2, c=1) == [1, 1]
+    roots = Solver.solve(1.0, 2.000000001, 1.0)
+    assert roots[0] == roots[1]
+
+
+def incorrect_values():
+    with pytest.raises(TypeError):
+        Solver.solve("1", 2, 1)
+        Solver.solve(b=2, c=1)
+        Solver.solve(a=1, b={1: "test_value"}, c=-1)
+        Solver.solve(a=1, b=0, c=[-1, 2])
